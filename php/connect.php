087d4f9df3d47f23a9c3adf27f2f04d5da3cf300
lew3l1 <slimka1213@gmail.com>
2024-09-23 14:39:59 +0500

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,13 +1,12 @@
-<?php
-$host = '127.0.0.1';
-$dbname = 'repeat';
-$user = 'root'; // Замените на ваше имя пользователя базы данных
-$pass = ''; // Замените на ваш пароль базы данных
+    <?php
+    $host = '127.0.0.1';
+    $dbname = 'repeat';
+    $user = 'root'; 
+    $pass = ''; 
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
-}
-?>
+    $conn = new mysqli($host, $user, $pass, $dbname);
+
+    if ($conn->connect_error) {
+        die("Ошибка подключения: " . $conn->connect_error);
+    }
+    ?>

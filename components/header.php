9c6770d3ee470fe003b6d2e2b60fcde8d242fc8b
lew3l1 <slimka1213@gmail.com>
2024-09-23 14:48:34 +0500

    Update header.php

--- components/header.php
+++ components/header.php
@@ -9,7 +9,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Universal</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/header.css">
 </head>
 <body>
     <header>

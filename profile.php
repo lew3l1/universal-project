00b49b4429c42e321a1ecccece6de12fc2f0bd69
lew3l1 <slimka1213@gmail.com>
2024-09-23 14:39:40 +0500

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,20 @@
+<?php
+require_once "php/connect.php";
+session_start();
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+</head>
+<body>
+
+ <?php require_once "components/header.php" ?>
+    
+</body>
+</html>
